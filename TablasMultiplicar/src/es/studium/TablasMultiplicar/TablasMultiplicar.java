a92de3b6b2a3f7373dd03e8eb15d9f9e14c7431c
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2018-03-06 18:20:56 +0100

    TablasMultiplicar

---
+++ TablasMultiplicar/src/es/studium/TablasMultiplicar/TablasMultiplicar.java
@@ -0,0 +1,20 @@
+package es.studium.TablasMultiplicar;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class TablasMultiplicar
+{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader lectura = new BufferedReader(new
+				InputStreamReader(System.in));
+		int numero,resultado,i;
+		System.out.println("Dame un número:");
+		numero = Integer.parseInt(lectura.readLine());
+		for (i=0;i<=10;i++)
+		{
+			resultado=numero*i;
+			System.out.println(numero+"*"+i+"="+resultado);
+		}
+	}
+}
